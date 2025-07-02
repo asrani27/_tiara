fc4cb0f056a54cebd7bec297e5e49b92c6e95e92
asrani <asrani.27@gmail.com>
2025-07-02 17:11:47 +0800

    f

---
+++ resources/views/layouts/menu_foreman.blade.php
@@ -0,0 +1,57 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="/foreman/home" class="nav-link {{ Request::is('foreman/home*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-home"></i>
+                <p>
+                    Beranda
+                </p>
+            </a>
+        </li>
+        <li class="nav-header">DATA UTAMA</li>
+
+        <li class="nav-item">
+            <a href="/foreman/penunjukan" class="nav-link {{ Request::is('foreman/penunjukan*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-file"></i>
+                <p>
+                    Surat Penunjukan
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="/foreman/pengajuan" class="nav-link {{ Request::is('foreman/pengajuan*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-file"></i>
+                <p>
+                    Surat Pengajuan
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-header">SETTING</li>
+        <li class="nav-item">
+            <a href="/foreman/laporan" class="nav-link">
+                <i class="nav-icon fas fa-file"></i>
+                <p>
+                    Laporan
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/foreman/monitoring" class="nav-link">
+                <i class="nav-icon fas fa-file"></i>
+                <p>
+                    Monitoring
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/logout" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>
+                    Logout
+                </p>
+            </a>
+        </li>
+    </ul>
+</nav>
